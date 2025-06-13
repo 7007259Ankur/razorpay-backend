9f4c0ebf3ccb38b2086867de645b7ea4fc7f98d6
7007259Ankur <ankurkumar7753@gmail.com>
2025-06-13 07:32:14 +0530

    Second change

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ connectDB();
 
 export const instance = new Razorpay({
   key_id: process.env.RAZORPAY_API_KEY,
-  key_secret: process.env.RAZORPAY_APT_SECRET,
+  key_secret: process.env.RAZORPAY_API_SECRET, // ✅ FIXED HERE
 });
 
 app.listen(process.env.PORT, () =>
